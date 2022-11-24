c81e14d5bea278884f8044966485dac73d94bd09
yassoub17 <114566887+yassoub17@users.noreply.github.com>
2022-11-24 15:50:19 +0100

    Create program.js

---
+++ javascript/ex00/program.js
@@ -0,0 +1,14 @@
+function pair(nombre){
+  if(nombre%2 == 0)
+              {
+                console.log("Even");
+                     return("Even");
+              }
+              else
+              {
+                  console.log("Odd");
+                     return("Odd");
+              }
+         }
+
+pair(14);
