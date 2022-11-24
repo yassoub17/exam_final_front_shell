3a7fdb966bcd488cb784a51ac8748d25fae26d97
yassoub17 <114566887+yassoub17@users.noreply.github.com>
2022-11-24 15:51:48 +0100

    Create program.js

---
+++ javascript/ex01/program.js
@@ -0,0 +1,22 @@
+function oppositeNumber(nombre){
+
+  if (nombre ==0 ){
+    console.log(0)
+      return 0
+  }
+
+    if (nombre< 0){
+      console.log(-nombre)
+        return -nombre;
+    
+}
+   else {
+     console.log(-nombre)
+        return -nombre; 
+         
+    }
+
+}
+
+
+oppositeNumber(0)
